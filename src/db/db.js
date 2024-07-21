e7b99c7be0f4627c8866b7c8049285c0320c0ffc
vishalpatilmaske <vishpatilmaske@gmail.com>
2024-07-21 22:13:37 +0530

    make some changes

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      sslValidate: false,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
+
 export default connectDB;
